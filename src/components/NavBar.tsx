4b16f77880783a890ad20663586bf5700902782e
Joaquin <elhombrej@hotmail.com>
2024-02-23 18:49:06 -0300

    fix: reload

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -20,24 +20,6 @@ import MenuItem from "@mui/material/MenuItem";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import "react-slideshow-image/dist/styles.css";
 
-const pathSegments = window.location.pathname.split("/");
-
-// Seleccionar los segmentos deseados
-let desiredPathSegment = "";
-let desiredLanguage = "";
-
-if (pathSegments.length >= 3) {
-  desiredLanguage = pathSegments[2];
-  desiredPathSegment = "/" + pathSegments[1];
-} else if (pathSegments.length >= 2) {
-  desiredLanguage = pathSegments[1];
-}
-
-// Si no hay segmentos o si el idioma no se establece, establecer español como valor predeterminado
-if (!desiredLanguage) {
-  desiredLanguage = "uy";
-}
-
 interface Props {
   /**
    * Injected by the documentation to work in an iframe.
@@ -64,84 +46,23 @@ function HideOnScroll(props: Props) {
 }
 
 function NavBar(props: Props) {
-  const ImageButton = styled(ButtonBase)(({ theme }) => ({
-    position: "relative",
-    height: 200,
-    [theme.breakpoints.down("sm")]: {
-      width: "100% !important", // Overrides inline-style
-      height: 100,
-    },
-    "&:hover, &.Mui-focusVisible": {
-      zIndex: 1,
-      "& .MuiImageBackdrop-root": {
-        opacity: 0.15,
-      },
-      "& .MuiImageMarked-root": {
-        opacity: 0,
-      },
-      "& .MuiTypography-root": {
-        border: "4px solid currentColor",
-      },
-    },
-  }));
+  const pathSegments = window.location.pathname.split("/");
 
-  const ImageSrc = styled("span")({
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundSize: "cover",
-    backgroundPosition: "center 40%",
-  });
+  // Seleccionar los segmentos deseados
+  let desiredPathSegment = "";
+  let desiredLanguage = "";
 
-  const Image = styled("span")(({ theme }) => ({
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    color: theme.palette.common.white,
-  }));
+  if (pathSegments.length >= 3) {
+    desiredLanguage = pathSegments[2];
+    desiredPathSegment = "/" + pathSegments[1];
+  } else if (pathSegments.length >= 2) {
+    desiredLanguage = pathSegments[1];
+  }
 
-  const ImageBackdrop = styled("span")(({ theme }) => ({
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    backgroundColor: theme.palette.common.black,
-    opacity: 0.4,
-    transition: theme.transitions.create("opacity"),
-  }));
-
-  const ImageMarked = styled("span")(({ theme }) => ({
-    height: 3,
-    width: 18,
-    backgroundColor: theme.palette.common.white,
-    position: "absolute",
-    bottom: -2,
-    left: "calc(50% - 9px)",
-    transition: theme.transitions.create("opacity"),
-  }));
-
-  const points = [
-    {
-      latitude: -34.900648821436256,
-      longitude: -56.14527830948932,
-      name: "Piwo Villa Dolores",
-      image: piwovilladolores,
-    },
-    {
-      latitude: -34.90828748640591,
-      longitude: -56.208099671234436,
-      name: "Piwo Ciudad Vieja",
-      image: piwociudadvieja,
-    },
-  ];
+  // Si no hay segmentos o si el idioma no se establece, establecer español como valor predeterminado
+  if (!desiredLanguage) {
+    desiredLanguage = "uy";
+  }
 
   const StyledMenu = styled((props: MenuProps) => (
     <Menu
